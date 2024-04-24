946c91fa7dff823af426f86406dd6ec20b987120
ballursajeed <ballursajeed@gmail.com>
2024-04-24 19:32:27 +0530

    Added Authorization for getting user or deleting user

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { deleteUser, getAllUsers, getUser, newUser } from "../controllers/user.js";
+import { AdminOnly } from "../middlewares/auth.js";
 
 const app = express.Router();
 
@@ -7,10 +8,10 @@ const app = express.Router();
  app.post("/new",newUser)
 
  // route - /api/v1/user/all
- app.get("/all",getAllUsers)
+ app.get("/all",AdminOnly,getAllUsers)
 
  // route - /api/v1/user/dynamicID
- app.route("/:id").get(getUser).delete(deleteUser)
+ app.route("/:id").get(getUser).delete(AdminOnly,deleteUser)
 
 
 export default app;
