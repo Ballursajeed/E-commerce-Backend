38f8f8a12fd7e2fd1a97f1d704ac246d383d914f
ballursajeed <ballursajeed@gmail.com>
2024-04-28 16:05:26 +0530

    Created /all-coupon and /delete coupon

--- dist/routes/payment.js
+++ dist/routes/payment.js
@@ -1,6 +1,8 @@
 import express from "express";
-import { applyDiscounts, newCoupon } from "../controllers/payment.js";
+import { allCoupons, applyDiscounts, deleteCoupon, newCoupon } from "../controllers/payment.js";
 const app = express.Router();
 app.post("/coupon/new", newCoupon);
 app.get("/discount", applyDiscounts);
+app.get("/coupon/all", allCoupons);
+app.delete("/coupon/:id", deleteCoupon);
 export default app;
--- src/controllers/payment.ts
+++ src/controllers/payment.ts
@@ -28,3 +28,27 @@ export const applyDiscounts = TryCath(async (req, res, next) => {
     discount: discount.amount,
   });
 });
+
+export const allCoupons = TryCath(async (req, res, next) => {
+
+ const coupons = await Coupon.find({})
+
+  return res.status(200).json({
+    success: true,
+    coupons
+  });
+});
+
+export const deleteCoupon = TryCath(async (req, res, next) => {
+
+  const {id} = req.params;
+
+  const coupon = await Coupon.findByIdAndDelete(id)
+
+  if(!coupon) return next(new ErrorHandler("Invalid ID",400))
+ 
+   return res.status(200).json({
+     success: true,
+     message:`Coupon ${coupon?.code} Deleted Successfully`
+   });
+ });
