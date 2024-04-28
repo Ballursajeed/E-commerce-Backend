a719ca91b8e7e90695d71b5ae01cf13d12d40112
ballursajeed <ballursajeed@gmail.com>
2024-04-28 15:25:36 +0530

    Created /Coupon for new Coupon

---
+++ src/controllers/payment.ts
@@ -0,0 +1,18 @@
+import { TryCath } from "../middlewares/error.js";
+import { Coupon } from "../models/coupon.js";
+import ErrorHandler from "../utils/utility-class.js";
+
+export const newCoupon = TryCath(async (req, res, next) => {
+  const { coupon, amount } = req.body;
+
+  if(!coupon || !amount) return next(new ErrorHandler("Please Enter Both Coupon and Amount",400))
+
+  await Coupon.create({ code: coupon, amount });
+
+  return res.status(201).json({
+    success:true,
+    message:`Coupon ${coupon} Created successfully`
+  })
+
+});
+
---
+++ src/routes/payment.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { AdminOnly } from "../middlewares/auth.js";
+import { newCoupon } from "../controllers/payment.js";
+
+const app = express.Router();
+
+// route - /api/v1/user/new
+ app.post("/coupon/new",newCoupon)
+
+export default app;
