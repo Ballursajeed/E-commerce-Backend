232f9e2cd4f615c346d64d5a466d1f8e99a1ddf2
Sajeed Balluru <123645836+Ballursajeed@users.noreply.github.com>
2024-05-20 14:14:11 +0530

    Delete dist directory

--- dist/routes/stats.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { AdminOnly } from "../middlewares/auth.js";
-import { getBarStats, getDashboardStats, getLineStats, getPieStats } from "../controllers/stats.js";
-const app = express.Router();
-app.get("/stats", AdminOnly, getDashboardStats);
-app.get("/pie", AdminOnly, getPieStats);
-app.get("/bar", AdminOnly, getBarStats);
-app.get("/line", AdminOnly, getLineStats);
-export default app;
